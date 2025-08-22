69fc0ec9273e09236c02b91a212612898f819af8
abhayjumhare-20 <abhayjumhare20@gmail.com>
2025-08-22 23:57:17 +0530

    Message Encryption And Decryption

---
+++ SECRET CODE  LANG.py
@@ -0,0 +1,100 @@
+import random
+import string
+
+def encrypt_message(message):
+    """
+    Encrypts each word in the given message based on the following rules:
+    1. If a word has fewer than 3 characters, simply reverse it.
+    2. If a word has 3 or more characters:
+       - Swap the first and last letter.
+       - Add three random letters at the beginning and end of the word.
+    """
+    
+    words = message.split()  # Split the input into words
+    encrypted_words = []  # List to store encrypted words
+    
+    for word in words:
+        msg = list(word)  # Convert word to a list of characters
+        
+        if len(word) < 3:
+            msg.reverse()  # Reverse if length is less than 3
+            encrypted_words.append(''.join(msg))  # Convert back to string and store
+        else:
+            msg[0], msg[-1] = msg[-1], msg[0]  # Swap first and last character
+            
+            # Generate random letters for padding
+            random_letters_start = ''.join(random.choices(string.ascii_letters, k=3))
+            random_letters_end = ''.join(random.choices(string.ascii_letters, k=3))
+            
+            # Construct the encrypted word and store it
+            encrypted_words.append(random_letters_start + ''.join(msg) + random_letters_end)
+    
+    return ' '.join(encrypted_words)  # Join the encrypted words back into a string
+
+
+message = str(input("Enter A Message : "))  
+print("Encrypted Message:", encrypt_message(message))
+
+#=========================================================================================================================================================================================================================================================
+# Decryption :=>
+
+
+def decrypt_message(encrypted_message):
+    """
+    Decrypts each word in the given encrypted message based on the rules:
+    1. If a word has fewer than 3 characters, simply reverse it.
+    2. If a word has 9 or more characters (3 random + original word + 3 random):
+       - Remove the 3 letters at the start and end.
+       - Swap the first and last letters back.
+    """
+    words = encrypted_message.split()  # Split the message into words
+    decrypted_words = []  # List to store decrypted words
+
+    for word in words:
+        if len(word) < 3:
+            decrypted_words.append(word[::-1])  # Reverse for short words >> word[start:stop:step]
+        else:
+            core = word[3:-3]  # Remove the random letters
+            core = list(core) # then make them into list
+            core[0], core[-1] = core[-1], core[0]  # Swap first and last back
+            decrypted_words.append(''.join(core))  # convert msg into string format
+
+    return ' '.join(decrypted_words)  # Join words into a string
+
+
+encrypted_message = input("Enter Encrypted Message: ")
+print("Decrypted Message:", decrypt_message(encrypted_message))
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import random
+# import string
+# # ENCRYPTION
+# message = str(input("Enter A Message : "))
+# msg = list(message)
+# print("Original message as list:", msg)
+
+# if len(message) < 3:
+#     msg.reverse()
+#     print("Reversed message:", msg)
+# else:
+#     msg[0], msg[-1] = msg[-1], msg[0]
+#     print("Swapped message:", msg)
+#     # msg = list(message)
+#     random_letters_start = ''.join(random.choices(string.ascii_letters, k=3))
+#     random_letters_end = ''.join(random.choices(string.ascii_letters, k=3))
+#     msg = list(random_letters_start) + msg + list(random_letters_end)
+#     print("Modified message:", msg)
+#     l = str(msg)
+#     print(l)
+
